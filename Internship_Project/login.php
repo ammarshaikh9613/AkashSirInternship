2aa18d703089949efed62f1da037861e198b0c16
Ammar <58964666+ammarshaikh9613@users.noreply.github.com>
2021-06-04 22:07:42 +0530

    Login

--- Internship_Project/login.php
+++ Internship_Project/login.php
@@ -1,3 +1,33 @@
+<?php
+session_start();
+//$connection = new mysqli("localhost","root","","fitness_db") ;
+$connection = new mysqli("localhost","root","","student_db") ;
+        
+        
+    if($_POST)
+    {
+        $email=$_POST['email_id'];
+        $password=$_POST['password'];
+        
+        $selectquery= mysqli_query($connection, "select * from  login where email_id='{$email}' and password='{$password}'") or die(mysqli_error($connection));
+        $count = mysqli_num_rows($selectquery);
+        $row = mysqli_fetch_array($selectquery);
+        
+        //echo $count;
+        if($count>0)
+        {
+           $_SESSION['admin_id']=$row['admin_id'];
+           $_SESSION['admin_name']=$row['admin_name'];
+           
+           header("location:home.php");
+        }
+        else
+        {
+            echo "<script>alert('Invalid credentials')</script>";
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,6 +48,7 @@
   <link rel="shortcut icon" href="images/favicon.png" />
 </head>
 
+
 <body>
   <div class="container-scroller d-flex">
     <div class="container-fluid page-body-wrapper full-page-wrapper d-flex">
@@ -30,33 +61,25 @@
               </div>
               <h4>Hello! let's get started</h4>
               <h6 class="font-weight-light">Sign in to continue.</h6>
-              <form class="pt-3">
+              <form class="pt-3" method="post">
                 <div class="form-group">
-                  <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Username">
+                  <input type="email" class="form-control form-control-lg" name="email_id" id="exampleInputEmail1" placeholder="Username">
                 </div>
                 <div class="form-group">
-                  <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password">
+                  <input type="password" class="form-control form-control-lg" name="password" id="exampleInputPassword1" placeholder="Password">
                 </div>
                 <div class="mt-3">
-                  <a class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" href="index.html">SIGN IN</a>
+                    <button type="submit" class="btn btn-primary btn-lg btn-block">
+                      <i class="mdi mdi-account"></i>                      
+                      Submit
+                    </button>
                 </div>
                 <div class="my-2 d-flex justify-content-between align-items-center">
-                  <div class="form-check">
-                    <label class="form-check-label text-muted">
-                      <input type="checkbox" class="form-check-input">
-                      Keep me signed in
-                    </label>
-                  </div>
+                  
                   <a href="#" class="auth-link text-black">Forgot password?</a>
                 </div>
-                <div class="mb-2">
-                  <button type="button" class="btn btn-block btn-facebook auth-form-btn">
-                    <i class="mdi mdi-facebook mr-2"></i>Connect using facebook
-                  </button>
-                </div>
-                <div class="text-center mt-4 font-weight-light">
-                  Don't have an account? <a href="register.php" class="text-primary">Create</a>
-                </div>
+                
+                
               </form>
             </div>
           </div>
